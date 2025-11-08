01af27e40818e7993dff99f8ea5cc313f41cccc1
sfbto <minhohonh@minhohonhgmailgit config --global user.email>
2025-11-08 11:54:27 +0700

    ci cd

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -99,22 +99,11 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  // async getID(res,req,next){
-  //   const products =await Product.findById(req.params.id);
-  //   res.status(200).json(products);
-  // }
-  async getID( res,req,next){
-    const products= await Product.findById(req.params.ids);
+  async getID(req,res,next){
+    const products = await Product.findById(req.params.id);
     res.status(200).json(products);
   }
-  // async getID(req, res, next){
-  //   const products = await Product.findById(req.params.id);
-  //   res.status(200).json(products);
-  // }
-// // cau 8
-//     async getID(req, res, next) {
-//       const products = await Product.findById(req.params.id);
-//       res.status(200).json(products);
-//     }
+
+
 }
 module.exports = ProductController;
